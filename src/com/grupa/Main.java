f849996f01e8fd08e1001588d70c0520a3c43278
veccv <veccv09@gmail.com>
2020-12-14 17:16:55 +0100

    Dodanie zawartości do pliku Main

--- src/com/grupa/Main.java
+++ src/com/grupa/Main.java
@@ -3,11 +3,12 @@ package com.grupa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Wczytano plik główny!");
+        int i = 0; // Dodana zawartość pliku!
+	System.out.println("Wczytano plik główny!");
         System.out.println(plik1.dodawanie(1, 1));
         System.out.println(plik2.odejmowanie(2, 1));
         System.out.println(plik3.wypisz());
         System.out.println(plik4.mnozenie(4, 2));
-
+	
     }
 }
